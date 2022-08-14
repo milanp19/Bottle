c81ab5c9796bbc47e0b8b49cafdbc36193cea8b0
Milan Paulson <milanpaulson8@gmail.com>
2022-08-14 17:38:14 +0530

    nice

---
+++ cogs/Genshin.py
@@ -0,0 +1,56 @@
+import discord
+from discord.ext import commands
+import os
+from random import choice
+from json import load
+from PIL import Image
+
+
+
+class Genshin(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+    
+
+    @commands.command()
+    async def roll(ctx):
+    
+        chars = os.listdir('../assets/data/characters/')
+        char = choice(chars)
+        
+
+        with open(f"../data/characters/{char}/en.json", 'r') as f1, Image.open(f"../images/characters/{char}/portrait") as l:
+
+            data = load(f1)
+
+            embed = discord.Embed(title = data['name'])
+
+            embed.add_field(name = 'Nation', value=data['nation'], inline = False)
+            embed.add_field(name = 'Vision', value=data['vision'], inline = False)
+            embed.add_field(name = 'Weapon', value=data['weapon'], inline = False)
+        
+        
+
+            #l.save('mm.jpg')
+            l  = l.convert("RGBA")
+
+            im = l.resize((round(l.size[0]*2), round(l.size[1]*2)))
+            
+            im.save("mm.png")
+            file=discord.File("mm.png", filename="mm.png")
+
+
+            embed.set_image(url = "attachment://mm.png")
+
+
+
+        
+        #embed.set_thumbnail(url = "")
+        embed.set_footer(icon_url = ctx.author.avatar_url, text = f"packed by {ctx.author}")
+        await ctx.send(embed=embed,file = file)
+
+
+
+
+async def setup(client):
+    await client.add_cog(Genshin(client))
