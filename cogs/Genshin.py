c740514fdb229ea71b9ed6a009d54cea9d530d93
Milan Paulson <milanpaulson8@gmail.com>
2022-08-18 12:54:09 +0530

    minor modification

--- cogs/Genshin.py
+++ cogs/Genshin.py
@@ -20,52 +20,35 @@ class Genshin(commands.Cog):
         chars = os.listdir("data/characters/")
         char = choice(chars)
         
-        try:
-            with open(f"data/characters/{char}/en.json", 'r') as f1, Image.open(f"images/characters/{char}/portrait") as l:
-
-                data = json.load(f1)
-
-                embed = discord.Embed(title = "Character", timestamp = ctx.message.created_at)
-                embed.add_field(name = 'Name', value=data['name'], inline = True)
-                embed.add_field(name = 'Vision', value=data['vision'], inline = True)
-                embed.add_field(name = 'Nation', value=data['nation'], inline = True)
-                embed.add_field(name = 'Weapon', value=data['weapon'], inline = True)
-                embed.add_field(name = 'Rarity', value=data['rarity'] * '⭐', inline = True)
-                try:
-                    if data['birthday']:
-                        embed.add_field(name = 'Birthday', value=data['birthday'][5:], inline = True)
-                except KeyError:
-                    embed.add_field(name = 'Birthday', value='N/A', inline = True)
-                embed.add_field(name = 'Description', value=data['description'], inline = True)
-                
-            
-                # l  = l.convert("RGBA")
-                # l.save("mm.png")
-                # file=discord.File("mm.png", filename="mm.png")
-                embed.set_image(url = f"attachment://images/characters/{char}/portrait.png")
-                embed.set_footer(icon_url = ctx.author.avatar.url, text = f"packed by {ctx.author}")
-                await ctx.send(embed=embed)#,file = file)
-        except FileNotFoundError:
-            with open(f"data/characters/{char}/en.json", 'r') as f1:
-
-                data = json.load(f1)
-
-                embed = discord.Embed(title = "Character")
-                embed.add_field(name = 'Name', value=data['name'], inline = True)
-                embed.add_field(name = 'Vision', value=data['vision'], inline = True)
-                embed.add_field(name = 'Nation', value=data['nation'], inline = True)
-                embed.add_field(name = 'Weapon', value=data['weapon'], inline = True)
-                embed.add_field(name = 'Rarity', value=data['rarity'] * '⭐', inline = True)
-                try:
-                    if data['birthday']:
-                        embed.add_field(name = 'Birthday', value=data['birthday'][5:], inline = True)
-                except KeyError:
-                    embed.add_field(name = 'Birthday', value='N/A', inline = True)
-                embed.add_field(name = 'Description', value=data['description'], inline = True)
         
+        with open(f"data/characters/{char}/en.json", 'r') as f1:
 
-                embed.set_footer(icon_url = ctx.author.avatar.url, text = f"packed by {ctx.author}")
-                await ctx.send(embed=embed)
+            data = json.load(f1)
+
+            embed = discord.Embed(title = "Character", timestamp = ctx.message.created_at)
+            embed.add_field(name = 'Name', value=data['name'], inline = True)
+            embed.add_field(name = 'Vision', value=data['vision'], inline = True)
+            embed.add_field(name = 'Nation', value=data['nation'], inline = True)
+            embed.add_field(name = 'Weapon', value=data['weapon'], inline = True)
+            embed.add_field(name = 'Rarity', value=data['rarity'] * '⭐', inline = True)
+            try:
+                if data['birthday']:
+                    embed.add_field(name = 'Birthday', value=data['birthday'][5:], inline = True)
+            except KeyError:
+                embed.add_field(name = 'Birthday', value='N/A', inline = True)
+            embed.add_field(name = 'Description', value=data['description'], inline = True)
+            
+        
+            # l  = l.convert("RGBA")
+            # l.save("mm.png")
+            # file=discord.File("mm.png", filename="mm.png")
+            try:
+                embed.set_image(url = f"attachment://images/characters/{char}/portrait.png")
+            except FileNotFoundError:
+                pass
+            embed.set_footer(icon_url = ctx.author.avatar.url, text = f"packed by {ctx.author}")
+            await ctx.send(embed=embed)#,file = file)
+        
 
 
 
