f1a802c4eb6780164b83c5a0f064a3ba8f588caa
Milan Paulson <milanpaulson8@gmail.com>
2022-08-18 22:36:02 +0530

    add weapon command

--- cogs/Genshin.py
+++ cogs/Genshin.py
@@ -24,30 +24,69 @@ class Genshin(commands.Cog):
         with open(f"data/characters/{char}/en.json", 'r') as f1:
 
             data = json.load(f1)
-
-            embed = discord.Embed(title = "Character", timestamp = ctx.message.created_at)
-            embed.add_field(name = 'Name', value=data['name'], inline = True)
-            embed.add_field(name = 'Vision', value=data['vision'], inline = True)
-            embed.add_field(name = 'Nation', value=data['nation'], inline = True)
-            embed.add_field(name = 'Weapon', value=data['weapon'], inline = True)
-            embed.add_field(name = 'Rarity', value=data['rarity'] * '⭐', inline = True)
             try:
-                if data['birthday']:
-                    embed.add_field(name = 'Birthday', value=data['birthday'][5:], inline = True)
-            except KeyError:
-                print("no birthday data")
-                embed.add_field(name = 'Birthday', value='N/A', inline = True)
-            embed.add_field(name = 'Description', value=data['description'], inline = True)
+                embed = discord.Embed(title = "Character", timestamp = ctx.message.created_at)
+                embed.add_field(name = 'Name', value=data['name'], inline = True)
+                embed.add_field(name = 'Vision', value=data['vision'], inline = True)
+                embed.add_field(name = 'Nation', value=data['nation'], inline = True)
+                embed.add_field(name = 'Weapon', value=data['weapon'], inline = True)
+                embed.add_field(name = 'Rarity', value=data['rarity'] * '⭐', inline = True)
+                try:
+                    if data['birthday']:
+                        embed.add_field(name = 'Birthday', value=data['birthday'][5:], inline = True)
+                except KeyError:
+                    print("no birthday data")
+                    embed.add_field(name = 'Birthday', value='N/A', inline = True)
+                embed.add_field(name = 'Description', value=data['description'], inline = True)
+                
             
+                # l  = l.convert("RGBA")
+                # l.save("mm.png")
+                try:
+                    file=discord.File(f"images/characters/{char}/portrait.png",filename="mm.png")
+                    embed.set_image(url = "attachment://mm.png")
+                except FileNotFoundError:
+                    file = None
+                    print(data['name'])
+                    print("file not found error")
+            except HTTPException:
+                print(data['name'])
+            embed.set_footer(icon_url = ctx.author.avatar.url, text = f"packed by {ctx.author}")
+            await ctx.send(embed=embed,file=file)
+
+    @commands.command()
+    async def weapon(self, ctx):
+    
         
-            # l  = l.convert("RGBA")
-            # l.save("mm.png")
+        weapons = os.listdir("data/weapons/")
+        weapon = choice(weapons)
+        
+        
+        with open(f"data/weapons/{weapon}/en.json", 'r') as f1:
+
+            data = json.load(f1)
             try:
-                file=discord.File(f"images/characters/{char}/portrait.png",filename="mm.png")
-                embed.set_image(url = "attachment://mm.png")
-            except FileNotFoundError:
-                file = None
-                print("file not found error")
+                embed = discord.Embed(title = "Weapon", timestamp = ctx.message.created_at)
+                embed.add_field(name = 'Name', value=data['name'], inline = True)
+                embed.add_field(name = 'Type', value=data['type'], inline = True)
+                embed.add_field(name = 'Base Attack', value=data['baseAttack'], inline = True)
+                embed.add_field(name = 'Sub Stat', value=data['subStat'], inline = True)
+                embed.add_field(name = 'Rarity', value=data['rarity'] * '⭐', inline = True)
+                embed.add_field(name = 'Location', value=data['location'], inline = True)
+                try:
+                    embed.add_field(name = f"Passive Name: {data['passiveName']}", value=data['passiveDesc'], inline = False)
+                except KeyError:
+                    print("weapon passive not available")
+
+                try:
+                    file=discord.File(f"images/weapons/{weapon}/icon.png",filename="yes.png")
+                    embed.set_thumbnail(url = "attachment://yes.png")
+                except FileNotFoundError:
+                    file = None
+                    print(data['name'])
+                    print("no weapon icon")
+            except HTTPException:
+                print(data['name'])
             embed.set_footer(icon_url = ctx.author.avatar.url, text = f"packed by {ctx.author}")
             await ctx.send(embed=embed,file=file)
         
---
+++ rename.py
@@ -0,0 +1,13 @@
+# import os 
+
+# files = os.scandir("images/weapons")
+# k = True
+# while k:
+#     try:
+#         for file in files:
+#             if file == None:
+#                 k = False
+            
+#             os.rename(f"images/weapons/{file.name}/icon", f"images/weapons/{file.name}/icon.png")
+#     except FileNotFoundError:
+#         pass
